3f8c1ae9428d1f2ae4f0c21098adc9b9ad2601bd
Erick Dávila <erick.e_davila@hotmail.com>
2018-06-08 23:31:28 -0600

    Cambios en enviroment

--- loginApp/src/environments/environment.prod.ts
+++ loginApp/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyB3_x5SQA-nzyRy_RYP0ZpR8pWGTH92GVY",
+    authDomain: "loginapp-f4028.firebaseapp.com",
+    databaseURL: "https://loginapp-f4028.firebaseio.com",
+    projectId: "loginapp-f4028",
+    storageBucket: "loginapp-f4028.appspot.com",
+    messagingSenderId: "548097146804"
+  }
 };
--- loginApp/src/environments/environment.ts
+++ loginApp/src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyB3_x5SQA-nzyRy_RYP0ZpR8pWGTH92GVY",
+    authDomain: "loginapp-f4028.firebaseapp.com",
+    databaseURL: "https://loginapp-f4028.firebaseio.com",
+    projectId: "loginapp-f4028",
+    storageBucket: "loginapp-f4028.appspot.com",
+    messagingSenderId: "548097146804"
+  }
 };
 
 /*
